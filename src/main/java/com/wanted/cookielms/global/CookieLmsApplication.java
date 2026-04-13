30d23892bb064ca39c7b963dbfb3ba86bf5765dd
suerovr <suerovr20@gmail.com>
2026-04-13 23:57:57 +0900

    set packages

---
+++ src/main/java/com/wanted/cookielms/global/CookieLmsApplication.java
@@ -0,0 +1,13 @@
+package com.wanted.cookielms.global;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookieLmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CookieLmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wanted/cookielms/CookieLmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wanted.cookielms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookieLmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
