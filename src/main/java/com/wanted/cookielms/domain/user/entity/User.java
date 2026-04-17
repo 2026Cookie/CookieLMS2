669d6afa0d5bef419ceb0ed8a080b9247902bb16
Dong_gu <dkdlwkr207@gmail.com>
2026-04-17 11:18:52 +0900

    fix. enum @Enumerated(EnumType.STRING) 으로 변경

--- src/main/java/com/wanted/cookielms/domain/user/entity/User.java
+++ src/main/java/com/wanted/cookielms/domain/user/entity/User.java
@@ -44,6 +44,7 @@ public class User {
     // eum 클래는 필드 순서를 따라 번호가 부여된다.
     // ordinal , string
     // userDetails 는 앤티티에 있으면 안된다, 수정 반드시 필요!!
+    @Enumerated(EnumType.STRING)
     private Role role;
     private Status status;
 
